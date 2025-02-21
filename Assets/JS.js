d689f2929775c35a3e26e959eb2790f82f8a7aed
gaurigawade200 <gaurigawade2000@gmail.com>
2025-02-21 16:21:21 +0530

    add footer

--- Assets/JS.js
+++ Assets/JS.js
@@ -2,11 +2,3 @@
         function disableRightClick(event) {
             event.preventDefault()
         }
-        function loadPage() {
-            fetch('about.html')
-                .then(response => response.text())
-                .then(data => {
-                    document.getElementById('dynamicContent').innerHTML = data;
-                })
-                .catch(error => console.error('Error loading page:', error));
-        }
